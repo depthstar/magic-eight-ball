49a345c0666c57c1d868577fb60a6e4469b9136d
depthstar <atempovivo@gmail.com>
2021-01-16 19:09:35 -0600

    added test array content -- will get more"

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -4,11 +4,12 @@
 const result = arr => arr[Math.floor(Math.random() * arr.length)];
 
 //Arrays with answer components
-const congratsArray = [];
-const sorryArray = [];
-const winArray = [];
-const loseArray = [];
-const tryAgainArray = [];
+const congratsArray = ['Nice!', 'Congrats!', 'Wow!'];
+const sorryArray = ['Ouch!', 'Sorry!', 'Too bad!'];
+const winArray = [`As I see it, yes.`, 'It is certain.', 'You may rely on it.'];
+const loseArray = [`Don't count on it.`, `Very doubtful.`, `My sources say no.`];
+const tryAgainwin = [`Don't get too cocky!`, `Stay the course!`, `Keep it up!`];
+const tryAgainlose = [`Better luck next time!`, `This too shall pass.`, `Keep your head up!`]
 
 //Create variable for final message
 let message;
@@ -20,14 +21,14 @@ if (result === 0) { //if "win"
     winIndex = Math.floor(Math.random() * winArray.length);
     tryAgainIndex = Math.floor(Math.random() * tryAgainArray.length);
     //generate message
-    message = congratsArray[congratsIndex] + winArray[winIndex] + tryAgainArray[tryAgainIndex];
+    message = congratsArray[congratsIndex] + winArray[winIndex] + tryAgainWin[tryAgainIndex];
 } else { //if "lose"
 //create indexes for losing and try again arrays
     sorryIndex = Math.floor(Math.random() * sorryArray.length);
     loseIndex = Math.floor(Math.random() * loseArray.length);
     tryAgainIndex = Math.floor(Math.random() * tryAgainArray.length);
     //generate message
-    message = sorryArray[sorryIndex] + loseArray[loseIndex] + tryAgainArray[tryAgainIndex];
+    message = sorryArray[sorryIndex] + loseArray[loseIndex] + tryAgainLose[tryAgainIndex];
 
 }
 
