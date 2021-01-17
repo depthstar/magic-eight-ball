2ac252b79c83137bfe24a8524f3f5d57a93afbb2
depthstar <atempovivo@gmail.com>
2021-01-16 19:02:18 -0600

    added message generation logic

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -13,9 +13,22 @@ const tryAgainArray = [];
 //Create variable for final message
 let message;
 
-//if "win"
-if (result === 0) {
-    
-    
+
+if (result === 0) { //if "win"
+//create indexes for winning and try again arrays
+    congratsIndex = Math.floor(Math.random() * congratsArray.length);
+    winIndex = Math.floor(Math.random() * winArray.length);
+    tryAgainIndex = Math.floor(Math.random() * tryAgainArray.length);
+    //generate message
+    message = congratsArray[congratsIndex] + winArray[winIndex] + tryAgainArray[tryAgainIndex];
+} else { //if "lose"
+//create indexes for losing and try again arrays
+    sorryIndex = Math.floor(Math.random() * sorryArray.length);
+    loseIndex = Math.floor(Math.random() * loseArray.length);
+    tryAgainIndex = Math.floor(Math.random() * tryAgainArray.length);
+    //generate message
+    message = sorryArray[sorryIndex] + loseArray[loseIndex] + tryAgainArray[tryAgainIndex];
 
 }
+
+console.log(message);
