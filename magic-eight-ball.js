fba06099b915c1431f4c659b8c314d19f330f5ad
depthstar <atempovivo@gmail.com>
2021-01-21 18:50:01 -0600

    added CSS, still need to get button to stay put

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -8,12 +8,8 @@
 
 
 //Arrays with answer components
-const congratsArray = ['Nice!', 'Congrats!', 'Wow!', 'Huzza!', 'Yippee', 'Hurray!', 'Yippee!'];
-const sorryArray = ['Ouch!', 'Sorry!', 'Too bad!', 'Oh no!', 'Sad day.', 'Awe!', 'Shoot!', 'Darn!'];
 const winArray = [`It's a slam dunk!`, 'From my angle, yes!', 'Seems likely.', '','It is certain.', 'You may rely on it.', 'Outlook good.', 'Most likely', 'Yes - definitely', 'Signs point to yes!'];
 const loseArray = [`Don't count on it.`, `Very doubtful.`, `My sources say no.`, `My reply is no`, `Outlook not so good`];
-const tryAgainWin = [`Don't get too cocky!`, `Stay the course!`, `Keep it up!`, 'Hope your luck remains!', 'This too shall pass'];
-const tryAgainLose = [`Better luck next time!`, `This too shall pass.`, `Keep your head up!`, `Don't quit now!`, `Everyone has a down day! Get some rest!`];
 const notSureArray = ['Reply hazy, try again.', 'Ask again later', 'Better not tell you now', 'Cannot predict now.', 'Concentrate and ask again.'];
 
 
@@ -33,20 +29,16 @@ const shakeEightBall = () => {
 
     if (result === true) {  //if "win" (50% odds)
             //create indexes for winning and try again arrays
-        let congratsIndex = generateRandIndex(congratsArray);
         let winIndex = generateRandIndex(winArray);
-        let tryAgainIndex = generateRandIndex(tryAgainWin);
             //generate message
-        message = congratsArray[congratsIndex] + " " + winArray[winIndex] +  " " + tryAgainWin[tryAgainIndex];
+        message = winArray[winIndex];
     } else {    //if "losing" (50% odds)
                 //create indexes for losing and try again arrays
         let hazy = Math.random() < 0.5;
         if (hazy === false) { //if lose (25% odds)
-        let sorryIndex = generateRandIndex(sorryArray);
         let loseIndex = generateRandIndex(loseArray);
-        let tryAgainIndex = generateRandIndex(tryAgainLose);
                 //generate message
-            message = sorryArray[sorryIndex] + " " + loseArray[loseIndex] + " " + tryAgainLose[tryAgainIndex];
+            message = loseArray[loseIndex];
         } else { //if non-committal (25%)
             let notSureIndex = generateRandIndex(notSureArray);
             message = notSureArray[notSureIndex]
@@ -54,6 +46,6 @@ const shakeEightBall = () => {
     
 
 }
-    document.getElementById("answer").innerHTML = 'Answer: ' + message;
+    document.getElementById("answer").innerHTML = message;
 }
 
